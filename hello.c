a96a1570a64bf12d1664010b7a46b02f22d337ba
Jack Steckler <jsteckler2@huskers.unl.edu>
2021-08-28 19:10:37 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Steckler
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack!\n");
+
+  return 0;
+}
